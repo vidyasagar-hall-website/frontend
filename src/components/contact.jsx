acd74eafa401a5ea6a4aa11796b739d9cc7a0a1e
Suraj Gupta <82136372+surajgupta3072@users.noreply.github.com>
2022-02-03 22:12:05 +0530

    Contact Page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,162 +1,67 @@
-import { useState } from 'react'
-import emailjs from 'emailjs-com'
+import React from "react";
+import './contact.css';
+import { faPhone, faEnvelope, faMapMarker } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const initialState = {
-  name: '',
-  email: '',
-  message: '',
-}
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState)
-
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setState((prevState) => ({ ...prevState, [name]: value }))
-  }
-  const clearState = () => setState({ ...initialState })
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(name, email, message)
-    emailjs
-      .sendForm(
-        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
-      )
-      .then(
-        (result) => {
-          console.log(result.text)
-          clearState()
-        },
-        (error) => {
-          console.log(error.text)
-        }
-      )
-  }
   return (
-    <div>
-      <div id='contact'>
-        <div className='container'>
-          <div className='col-md-8'>
-            <div className='row'>
-              <div className='section-title'>
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
-              </div>
-              <form name='sentMessage' validate onSubmit={handleSubmit}>
-                <div className='row'>
-                  <div className='col-md-6'>
-                    <div className='form-group'>
-                      <input
-                        type='text'
-                        id='name'
-                        name='name'
-                        className='form-control'
-                        placeholder='Name'
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className='help-block text-danger'></p>
-                    </div>
-                  </div>
-                  <div className='col-md-6'>
-                    <div className='form-group'>
-                      <input
-                        type='email'
-                        id='email'
-                        name='email'
-                        className='form-control'
-                        placeholder='Email'
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className='help-block text-danger'></p>
-                    </div>
-                  </div>
-                </div>
-                <div className='form-group'>
-                  <textarea
-                    name='message'
-                    id='message'
-                    className='form-control'
-                    rows='4'
-                    placeholder='Message'
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className='help-block text-danger'></p>
-                </div>
-                <div id='success'></div>
-                <button type='submit' className='btn btn-custom btn-lg'>
-                  Send Message
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className='col-md-3 col-md-offset-1 contact-info'>
-            <div className='contact-item'>
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className='fa fa-map-marker'></i> Address
-                </span>
-                {props.data ? props.data.address : 'loading'}
-              </p>
-            </div>
-            <div className='contact-item'>
-              <p>
-                <span>
-                  <i className='fa fa-phone'></i> Phone
-                </span>{' '}
-                {props.data ? props.data.phone : 'loading'}
-              </p>
-            </div>
-            <div className='contact-item'>
-              <p>
-                <span>
-                  <i className='fa fa-envelope-o'></i> Email
-                </span>{' '}
-                {props.data ? props.data.email : 'loading'}
-              </p>
-            </div>
-          </div>
-          <div className='col-md-12'>
-            <div className='row'>
-              <div className='social'>
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : '/'}>
-                      <i className='fa fa-facebook'></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : '/'}>
-                      <i className='fa fa-twitter'></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : '/'}>
-                      <i className='fa fa-youtube'></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id='footer'>
-        <div className='container text-center'>
-          <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
-          </p>
-        </div>
-      </div>
+    <div >
+      <section clas className="header-contact"></section>
+
+<section className="contact">
+<div className="content-contact">
+<h2>Contact Us</h2>
+</div>
+<div className="container-contact">
+<div className="contactInfo">
+  <div className="box">
+    <div className="icon"><FontAwesomeIcon icon={faMapMarker} /></div>
+    <div className="text">
+      <h3>Address</h3>
+      <p>IIT Kharagpur, West Bengal</p>
+    </div>
+  </div>
+
+  <div className="box">
+    <div className="icon"><FontAwesomeIcon icon={faPhone} /></div>
+    <div className="text">
+      <h3>Phone</h3>
+      <p>+91 1234 567 890</p>
+    </div>
+  </div>
+
+  <div className="box">
+    <div className="icon"> <FontAwesomeIcon icon={faEnvelope} /></div>
+    <div className="text">
+      <h3>Email</h3>
+      <p>vsh.iitkgp@gmail.com</p>
+    </div>
+  </div>
+</div>
+<div className="contactForm">
+  <form >
+    <h2>Send Message</h2>
+    <div className="inputBox">
+      <input type="text" required />
+      <span>Full Name</span>
+    </div>
+    <div className="inputBox">
+      <input type="email" required />
+      <span>Email</span>
+    </div>
+    <div className="inputBox">
+      <textarea name="" id="" cols="30" rows="10" required></textarea>
+      <span>Type your Message...</span>
+    </div>
+    <div className="inputBox">
+      <input type="submit" value="Send" className="send_but" />
+    </div>
+  </form>
+</div>
+</div>
+</section>
+
+
     </div>
   )
 }
